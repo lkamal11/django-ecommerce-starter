4190a7701e17a0d2dfe1c418249167312e5fc741
Kamal Karki <lkamal11@yahoo.com>
2025-08-25 17:23:40 -0400

    Pagniation added

--- store/views.py
+++ store/views.py
@@ -5,11 +5,20 @@ from django.contrib import messages
 from .models import Product, Category, Order, OrderItem
 from .cart import Cart
 from .forms import CheckoutForm
+from django.core.paginator import Paginator
 
 def home(request):
     categories = Category.objects.all().order_by('name')
-    products = Product.objects.filter(in_stock=True).order_by('-created_at')[:8]
-    return render(request, 'store/home.html', {'categories': categories, 'products': products})
+    products = Product.objects.filter(in_stock=True).order_by('-created_at')
+    paginator = Paginator(products, 12)  # 12 products per page
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "store/home.html", {
+        "categories": categories,
+        "page_obj": page_obj,
+    })
 
 def product_list(request, category_slug=None):
     category = None
@@ -18,7 +27,22 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'store/product_list.html', {'category': category, 'categories': categories, 'products': products})
+
+    # Apply pagination
+    paginator = Paginator(products.order_by('-created_at'), 12)  # 12 per page
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'store/product_list.html',
+        {
+            'category': category,
+            'categories': categories,
+            'page_obj': page_obj,
+        }
+    )
+
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
